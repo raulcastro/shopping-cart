17ac63da784ab605798bea0e93c6713fc1d37c71
Raul Castro <Raul Castro>
2014-06-19 12:56:47 -0500

    back-end first view

--- databases/create-database.sql
+++ databases/create-database.sql
@@ -8,6 +8,40 @@ GRANT ALL PRIVILEGES ON db161140_bzr.* TO 'db161140_2go'@'localhost' IDENTIFIED 
 
 USE db161140_bzr;
 
+--
+-- Table structure for table `system_users`
+--
+CREATE TABLE `users` (
+    `user_id`   int(10) NOT NULL AUTO_INCREMENT,
+    `user`      varchar(30) NOT NULL,
+    `password`  char(40) NOT NULL,
+    `type`      int(1) NOT NULL,
+    PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_detail` (
+    `detail_id` int(10) NOT NULL AUTO_INCREMENT,
+    `user_id`   int(10) NOT NULL,
+    `name`      varchar(255) NOT NULL,
+    `email`     varchar(255) DEFAULT NULL,
+    `about`     text,
+    `avatar`    varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`detail_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_type` (
+    `type_id`   int(10) NOT NULL AUTO_INCREMENT,
+    `type`      varchar(50) NOT NULL,
+    PRIMARY KEY (`type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+INSERT INTO user_type(type) VALUES('administrator');
+
+INSERT INTO users(user, password, type) VALUES('administrator', SHA1('password'), 1);
+
+INSERT INTO user_detail(user_id, name, email) VALUES(1, 'Frederich Gauss', 'info@bazarenlinea.com.mx');
+
 CREATE TABLE app_info(
     title           VARCHAR(512) NULL,
     site_name       VARCHAR(512) NULL,
