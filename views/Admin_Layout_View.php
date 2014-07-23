e7737e9bc429578c3011e546a9cc056fbe2fe476
Raul Castro <Raul Castro>
2014-07-23 01:07:08 -0500

    firs backend layout =) ohh hell yeah!!!

--- views/Admin_Layout_View.php
+++ views/Admin_Layout_View.php
@@ -119,21 +119,15 @@ class Admin_Layout_View
 		ob_start();
 		?>
 		<?php echo self::getTopArea(); ?>
+		<?php echo self::getSideBar(); ?>
+		<?php echo self::getMainAlert(); ?>
+		<section class="content">
+			<?php echo self::getSectionsContent(); ?>
+		</section>
 		<?php
 		$body = ob_get_contents();
 		ob_end_clean();
 		return $body;
-	}
-	
-	public function getSideBar()
-	{
-		ob_start();
-		?>
-		
-		<?php
-		$sideBar = ob_get_contents();
-		ob_end_clean();
-		return $sideBar;
 	}
 	
 	/**
@@ -149,81 +143,14 @@ class Admin_Layout_View
 		?>
 		<div class="testing">
 			<header class="main">
-			    <h1><strong>Bazar en linea</strong> Dashboard</h1>
+			    <h1><strong><?php echo $this->data['appInfo']['siteName']; ?></strong> Dashboard</h1>
 			    <input type="text" value="search" />
 			</header>
 			<section class="user">
 			    <div class="profile-img">
 			        <p><img src="images/uiface2.png" alt="" height="40" width="40" /> Welcome back John Doe</p>
 			    </div>
-			    <div class="buttons">
-			        <button class="ico-font">&#59157;</button>
-			        <span class="button dropdown">
-			            <a href="#">Notifications <span class="pip">4</span></a>
-			            <ul class="notice">
-			                <li>
-			                    <hgroup>
-			                        <h1>You have a new task</h1>
-			                        <h2>Report web statistics week by week.</h2> 
-			                    </hgroup>
-			                    <p><span>14:24</span></p>
-			                </li>
-			                <li>
-			                    <hgroup>
-			                        <h1>New comment</h1>
-			                        <h2>Comment on <em>About page</em> by Darren.</h2> 
-			                    </hgroup>
-			                    <p><span>11:04</span></p>
-			                </li>
-			                <li>
-			                    <hgroup>
-			                        <h1>Broken link</h1>
-			                        <h2>We've spotted a broken link on the <em>Blog page</em>.</h2> 
-			                    </hgroup>
-			                    <p><span>10:46</span></p>
-			                </li>
-			                <li>
-			                    <hgroup>
-			                        <h1>User report</h1>
-			                        <h2><em>Lee Grant</em> has been promoted to admin.</h2> 
-			                    </hgroup>
-			                    <p><span>09:57</span></p>
-			                </li>
-			            </ul>
-			        </span> 
-			        <span class="button dropdown">
-			            <a href="#">Inbox <span class="pip">6</span></a>
-			            <ul class="notice">
-			                <li>
-			                    <hgroup>
-			                        <h1>Hi, I need a favour</h1>
-			                        <h2>John Doe</h2>
-			                        <h3>Lorem ipsum dolor sit amet, consectetuer sed aidping putamus delo de sit felume...</h3>
-			                    </hgroup>
-			                    <p><span>11:24</span></p>
-			                </li>
-			                <li>
-			                    <hgroup>
-			                        <h1><span class="icon">&#59154;</span>Hi, I need a favour</h1>
-			                        <h2>John Doe</h2>
-			                        <h3>Lorem ipsum dolor sit amet, consectetuer sed aidping putamus delo de sit felume...</h3>
-			                    </hgroup>
-			                    <p><span>11:24</span></p>
-			                </li>
-			                <li>
-			                    <hgroup>
-			                        <h1><span class="icon">&#59154;</span>Hi, I need a favour</h1>
-			                        <h2>John Doe</h2>
-			                        <h3>Lorem ipsum dolor sit amet, consectetuer sed aidping putamus delo de sit felume...</h3>
-			                    </hgroup>
-			                    <p><span>11:24</span></p>
-			                </li>
-			            </ul>
-			        </span> 
-			        <span class="button">Live</span>
-			         <span class="button">Help</span>
-			         <span class="button blue"><a href="index.html">Logout</a></span>
-			    </div>
+			    <?php echo self::getTopNotifications(); ?>
 			</section>
 		</div>
 		<?php
@@ -232,18 +159,242 @@ class Admin_Layout_View
 		return $sideBar;
 	}
 	
+	/**
+	 * getTopNotifications
+	 * 
+	 * it shows the last notifications, like inbox, and others
+	 * 
+	 * @return string
+	 */
 	
-	
-	public function getUserDetails()
+	public function getTopNotifications()
 	{
 		ob_start();
 		?>
-			
+		<div class="buttons">
+	        <button class="ico-font">&#59157;</button>
+	        <span class="button dropdown">
+	            <a href="#">Notifications <span class="pip">4</span></a>
+	            <ul class="notice">
+	                <li>
+	                    <hgroup>
+	                        <h1>You have a new task</h1>
+	                        <h2>Report web statistics week by week.</h2> 
+	                    </hgroup>
+	                    <p><span>14:24</span></p>
+	                </li>
+	            </ul>
+	        </span> 
+	        <span class="button dropdown">
+	            <a href="#">Inbox <span class="pip">6</span></a>
+	            <ul class="notice">
+	                <li>
+	                    <hgroup>
+	                        <h1>Hi, I need a favour</h1>
+	                        <h2>John Doe</h2>
+	                        <h3>Lorem ipsum dolor sit amet, consectetuer sed aidping putamus delo de sit felume...</h3>
+	                    </hgroup>
+	                    <p><span>11:24</span></p>
+	                </li>
+	            </ul>
+	        </span> 
+	        <span class="button">Live</span>
+	         <span class="button">Help</span>
+	         <span class="button blue"><a href="index.html">Logout</a></span>
+	    </div>
+		<?php
+		$notifications = ob_get_contents();
+		ob_end_clean();
+		return $notifications;
+	}
+	
+	/**
+	 * getSideBar
+	 *
+	 * it's the main menu of the background
+	 * @return string
+	 */
+	public function getSideBar()
+	{
+		ob_start();
+		?>
+		<nav>
+		    <ul>
+		        <li><a href="dashboard.html"><span class="icon">&#59176;</span> Dashboard</a></li>
+		        <li class="section">
+		            <a href="pages-table.html"><span class="icon">&#128196;</span> Sections</a>
+		            <ul class="submenu">
+		                <li><a href="page-new.html">Create section</a></li>
+		                <li><a href="page-timeline.html">View sections</a></li>
+		            </ul>   
+		        </li>
+		        <li>
+		            <a href="files.html"><span class="icon">&#127748;</span> Sliders <span class="pip">2</span></a>
+		            <ul class="submenu">
+		                <li><a href="files-upload.html">New slider</a></li>
+		                <li><a href="files.html">View sliders</a></li>
+		            </ul>
+		        </li>
+		        <li>
+		            <a href="blog-timeline.html"><span class="icon">&#59160;</span> Produtcs <span class="pip">12</span></a>
+		            <ul class="submenu">
+		                <li><a href="blog-new.html">New product</a></li>
+		                <li><a href="blog-table.html">All products</a></li>
+		            </ul>
+		        </li>
+		        <li>
+		            <a href="blog-timeline.html"><span class="icon">&#59160;</span> Blog <span class="pip">12</span></a>
+		            <ul class="submenu">
+		                <li><a href="blog-new.html">New post</a></li>
+		                <li><a href="blog-table.html">All posts</a></li>
+		                <li><a href="comments-timeline.html">View comments</a></li>
+		            </ul>
+		        </li>
+		        <li><a href="statistics.html"><span class="icon">&#128202;</span> Statistics</a></li>
+		        <li><a href="users.html"><span class="icon">&#128101;</span> Users <span class="pip">3</span></a></li>
+		        <li>
+		            <a href="ui-elements.html"><span class="icon">&#9881;</span> Settings</a>
+		            <ul class="submenu">
+		                <li><a href="icon-fonts.html">Icon fonts</a></li>
+		            </ul>
+		        </li>
+		    </ul>
+		</nav>
 		<?php
 		$sideBar = ob_get_contents();
 		ob_end_clean();
 		return $sideBar;
 	}
+	
+	/**
+	 * getMainAlert
+	 * 
+	 * it's the first option before the content
+	 * 
+	 * @return string
+	 */
+	public function getMainAlert()
+	{
+		ob_start();
+		?>
+		<section class="alert">
+		    <form method="link" action="page-new.html">
+		         <button class="green">Add new product</button>
+		    </form>
+		</section>
+		<?php
+		$alert = ob_get_contents();
+		ob_end_clean();
+		return $alert;
+	}
+	
+	public function getSectionsContent()
+	{
+		ob_start();
+		?>
+		<section class="widget">
+	        <header>
+	            <span class="icon">&#128196;</span>
+	            <hgroup>
+	                <h1>Pages</h1>
+	                <h2>CMS content pages</h2>
+	            </hgroup>
+	            <aside>
+	                <span>
+	                    <a href="#">&#9881;</a>
+	                    <ul class="settings-dd">
+	                        <li><label>Option a</label><input type="checkbox" /></li>
+	                        <li><label>Option b</label><input type="checkbox" checked="checked" /></li>
+	                        <li><label>Option c</label><input type="checkbox" /></li>
+	                    </ul>
+	                </span>
+	            </aside>
+	        </header>
+	        <div class="content">
+	            <table id="myTable" border="0" width="100">
+	                <thead>
+	                    <tr>
+	                        <th>Page title</th>
+	                        <th >Date</th>
+	                        <th>Child pages</th>
+	                        <th>Comments</th>
+	                        <th>Author</th>
+	                    </tr>
+	                </thead>
+	                    <tbody>
+	                        <tr>
+	                            <td><input type="checkbox" /> Home</td>
+	                            <td>01/3/2013</td>
+	                            <td>0</td>
+	                            <td>0</td>
+	                            <td>John Doe</td>
+	                        </tr>
+	                        <tr>
+	                            <td><input type="checkbox" /> Services</td>
+	                            <td>01/3/2013</td>
+	                            <td>4</td>
+	                            <td>0</td>
+	                            <td>John Doe</td>
+	                        </tr>
+	                        <tr>
+	                            <td><input type="checkbox" /> Portfolio</td>
+	                            <td>02/3/2013</td>
+	                            <td>12</td>
+	                            <td>0</td>
+	                            <td>John Doe</td>
+	                        </tr>
+	                        <tr>
+	                            <td><input type="checkbox" /> About us</td>
+	                            <td>02/3/2013</td>
+	                            <td>2</td>
+	                            <td>0</td>
+	                            <td>John Doe</td>
+	                        </tr>
+	                        <tr>
+	                            <td><input type="checkbox" /> Blog</td>
+	                            <td>02/3/2013</td>
+	                            <td>32</td>
+	                            <td>0</td>
+	                            <td>John Doe</td>
+	                        </tr>
+	                        <tr>
+	                            <td><input type="checkbox" /> Contact us</td>
+	                            <td>03/3/2013</td>
+	                            <td>0</td>
+	                            <td>0</td>
+	                            <td>John Doe</td>
+	                        </tr>
+	                        <tr>
+	                            <td><input type="checkbox" /> Our clients</td>
+	                            <td>04/3/2013</td>
+	                            <td>1</td>
+	                            <td>0</td>
+	                            <td>John Doe</td>
+	                        </tr>
+	                        <tr>
+	                            <td><input type="checkbox" /> Partnerships</td>
+	                            <td>04/3/2013</td>
+	                            <td>0</td>
+	                            <td>0</td>
+	                            <td>John Doe</td>
+	                        </tr>
+	                        <tr>
+	                            <td><input type="checkbox" />Jobs</td>
+	                            <td>04/3/2013</td>
+	                            <td>0</td>
+	                            <td>0</td>
+	                            <td>John Doe</td>
+	                        </tr>
+	                    </tbody>
+	                </table>
+	        </div>
+	    </section>
+		<?php
+		$sections = ob_get_contents();
+		ob_end_clean();
+		return $sections;
+	}
+	
 	
 	/**
 	 * getLoginSection
